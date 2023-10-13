20598ff639b7610db65633428cbba42d5c51e409
Fznaf <fauzan.afaisal@gmail.com>
2023-10-13 15:07:59 +0700

    fixed profile username display

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,14 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\AdoptionPlan;
+use Illuminate\Support\Facades\Crypt;
 
 class ProfileController extends Controller
 {
     public function index(){
         $user = Auth::user();
-        $adoptionPlans = $user->adoptionplans;
+        $cipher = "AES-128-ECB";
+        $secret = "fadhlanganteng123";
 
-        return view('profile', compact('adoptionPlans'));
+        $username = Auth::user()->name;
+        $username = openssl_decrypt($username, $cipher, $secret);
+        $adoptionPlans = $user->adoptionplans;
+        
+        return view('profile', compact('adoptionPlans', 'username'));
     }
 
     public function show()
