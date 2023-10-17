03fe040cc594da5590ad89d83a234aa04afa3062
Pascalrjt <pascal.rogerjt@gmail.com>
2023-10-17 22:41:44 +0700

    i mek animal alert pretti

--- resources/views/animals/index.blade.php
+++ resources/views/animals/index.blade.php
@@ -1,15 +1,27 @@
 @extends('master')
 @section('content')
+
+<style>
+.centered-alert {
+    position: absolute;
+    top: 10%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 20px; /* Adjust this value to your liking */
+    z-index: 9999;
+}
+</style>
+
     <h1>Data</h1>
-    <div class="row mx-5 justify-content-center">
-        @foreach ($animals as $item)
-            <div class="card mx-2 bg-dark text-white animal-card" style="width: 300px;">
-            @if (session()->has('success'))
-                <div class="alert alert-success alert-dismissible fade show small-font" role="alert">
+        @if (session()->has('success'))
+            <div class="alert alert-success alert-dismissible fade show small-font centered-alert" role="alert">
                  {{ session('success') }} <br>  
                 Animal name encrypted in: {{ session('animal_des_time_taken') }} ms (DES) <br>
             </div>
-            @endif
+        @endif
+    <div class="row mx-5 justify-content-center">
+        @foreach ($animals as $item)
+            <div class="card mx-2 bg-dark text-white animal-card" style="width: 300px;">
                 <a href="/animals/{{$item->id}}">
                     @if ($item->image)
                         <div style="width: 100%; height: 300px; margin: 10px 0; position: relative;">
