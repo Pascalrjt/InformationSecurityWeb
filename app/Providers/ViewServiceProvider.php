9459faa4d9e3177c0f149ad364aed59306c20ded
Pascalrjt <pascal.rogerjt@gmail.com>
2023-10-20 22:12:19 +0700

    fixed navbar name decryption

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -12,11 +12,13 @@ class ViewServiceProvider extends ServiceProvider
     {
         View::composer('*', function ($view) {
             if (Auth::check()) {
-                $cipher = "AES-128-ECB";
-                $secret = "fadhlanganteng12";
+                $cipher = "AES-256-CBC";
+                $options = 0;
+                $iv = str_repeat("0", openssl_cipher_iv_length($cipher));
 
                 $username = Auth::user()->name;
-                $username = openssl_decrypt($username, $cipher, $secret);
+                $username = openssl_decrypt($username, $cipher, Auth::user()->key, $options, $iv);
+                // $username = openssl_decrypt($username, $cipher, $secret);
                 $view->with('username', $username);
             }
         });
