3a136038688b4599fdbfbb6676a2a7a86bc79abb
Pibe <rafiadityawarman.lol@gmail.com>
2023-10-20 18:51:36 +0700

    sorry guys

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->text('image');
+            // $table->boolean('is_admin')->default(0);
+            $table->boolean('is_admin')->default(1);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        // Change the 'idcard' column data type to LONGTEXT using a raw SQL statement
+        DB::statement('ALTER TABLE users MODIFY image LONGTEXT');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
