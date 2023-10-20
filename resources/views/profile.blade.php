b2b5324fbd44b6b9db0d272c68d5c1c4dda14a5d
Pascalrjt <pascal.rogerjt@gmail.com>
2023-10-20 22:19:41 +0700

    fixed navbar name decryption

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,9 +2,12 @@
 @section('content')
 
 @php
-    $cipher = "AES-128-ECB";
-    $secret = "fadhlanganteng12";
-    $decryptedImage = openssl_decrypt(Auth::user()->image, $cipher, $secret);
+    $cipher = "AES-256-CBC";
+    $options = 0;
+    $iv = str_repeat("0", openssl_cipher_iv_length($cipher));
+    $decryptedImage = openssl_decrypt(Auth::user()->image, $cipher, Auth::user()->key, $options, $iv);
+    $decryptedEmail = openssl_decrypt(Auth::user()->email, $cipher, Auth::user()->key, $options, $iv);
+    // $decryptedImage = openssl_decrypt(Auth::user()->image, $cipher, $secret);
 @endphp
 <!-- <img src="data:image/png;base64,{{ $decryptedImage }}" alt="User Image"  style="width: 100%; height: 100%; object-fit: cover;"> -->
 
@@ -16,7 +19,7 @@
 
     <div class="container">
         <h1>{{ $username }}</h1>
-        <p>{{ Auth::user()->email }}</p>
+        <p>{{ $decryptedEmail }}</p>
         <p>{{ Auth::user()->bio }}</p>
         <div>
             <h2>ID Image</h2>
