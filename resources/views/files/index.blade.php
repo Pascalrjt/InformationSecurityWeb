dc280105faf59a82c8005bf553a4542d712fad11
Pascalrjt <pascal.rogerjt@gmail.com>
2023-11-20 08:31:02 +0700

    fixed user page to show users besides current user and made it so that the user has to be logged in to view user and file page
    Co-authored-by: Pascal <pascal.rogerjt@gmail.com>
    
    Co-authored-by: Fadhlan <works.fadhlan@gmail.com>

--- resources/views/files/index.blade.php
+++ resources/views/files/index.blade.php
@@ -1,23 +1,27 @@
 @extends('master')
 
 @section('content')
-    <h1>Uploaded Files</h1>
+    @auth
+        <h1>Uploaded Files</h1>
 
-    <div class="row mx-5 justify-content-center">
-        @foreach ($files->where('fileOwner', auth()->id()) as $file)
-            <div class="card mx-2 bg-dark text-white user-card" style="width: 400px; height: 100px;">
-                <div class="text-center" style="width: 100%; height: 300px; margin: 10px 0; position: relative;">
-                    {{-- <img src="{{ $file->url }}" alt="File Image" style="width: 100%; height: 100%; object-fit: cover;"> --}}
-                    <h5 class="mb-0 file-name">{{ $file->filename }}</h5>
-                    <a href="{{ $file->download_url }}" class="btn btn-primary mt-2">Download</a>
+        <div class="row mx-5 justify-content-center">
+            @foreach ($files->where('fileOwner', auth()->id()) as $file)
+                <div class="card mx-2 bg-dark text-white user-card" style="width: 400px; height: 100px;">
+                    <div class="text-center" style="width: 100%; height: 300px; margin: 10px 0; position: relative;">
+                        {{-- <img src="{{ $file->url }}" alt="File Image" style="width: 100%; height: 100%; object-fit: cover;"> --}}
+                        <h5 class="mb-0 file-name">{{ $file->filename }}</h5>
+                        <a href="{{ $file->download_url }}" class="btn btn-primary mt-2">Download</a>
+                    </div>
                 </div>
-            </div>
-        @endforeach
-    </div>
-
-    @if (auth()->check() && auth()->user()->is_admin)
-        <div class="d-flex mx-5 my-5 justify-content-center">
-            <a href="/files/create" class="btn btn-orange btn-info mx-5" style="max-width: 18rem;">+Add File</a>
+            @endforeach
         </div>
-    @endif
+
+        @if (auth()->user()->is_admin)
+            <div class="d-flex mx-5 my-5 justify-content-center">
+                <a href="/files/create" class="btn btn-primary btn-info mx-5" style="max-width: 18rem;">+Add File</a>
+            </div>
+        @endif
+    @else
+        <p>You need to be logged in to view this page. Please <a href="/login">login</a>.</p>
+    @endauth
 @endsection
--- resources/views/userlist/index.blade.php
+++ resources/views/userlist/index.blade.php
@@ -1,36 +1,40 @@
 @extends('master')
 @section('content')
 
-<style>
-.centered-alert {
-    position: absolute;
-    top: 10%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    font-size: 20px; /* Adjust this value to your liking */
-    z-index: 9999;
-}
-</style>
+@if(Auth::check())
+    <style>
+    .centered-alert {
+        position: absolute;
+        top: 10%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 20px; /* Adjust this value to your liking */
+        z-index: 9999;
+    }
+    </style>
 
-<h1>User List</h1>
-<div class="row mx-5 justify-content-center">
-    @foreach ($users as $user)
-        <div class="card mx-2 bg-dark text-white user-card" style="width: 400px; height: 400px;">
-                <div class="text-center" style="width: 100%; height: 300px; margin: 10px 0; position: relative;">
-                    <?php
-                        $cipher = "AES-256-CBC";
-                        $options = 0;
-                        $iv = str_repeat("0", openssl_cipher_iv_length($cipher));
-                        $encryptionKey = $user->keyAES;
-                        $decryptedName = openssl_decrypt($user->name, $cipher, $encryptionKey, $options, $iv);
-                        $decryptedImageAES = openssl_decrypt($user->imageBase64AES, $cipher, $encryptionKey, $options, $iv);
-                    ?>
-                    <img src="data:image/png;base64,{{ $decryptedImageAES }}" alt="User Image" style="width: 100%; height: 100%; object-fit: cover;">
-                    <h5 class="mb-0 user-name">{{$decryptedName}}</h5>
-                    <button class="btn btn-primary mt-2">Request Files</button>
-                </div>
-        </div>
-    @endforeach
-</div>
+    <h1>User List</h1>
+    <div class="row mx-5 justify-content-center">
+        @foreach ($users->where('id', '!=', Auth::id()) as $user)
+            <div class="card mx-2 bg-dark text-white user-card" style="width: 400px; height: 400px;">
+                    <div class="text-center" style="width: 100%; height: 300px; margin: 10px 0; position: relative;">
+                        <?php
+                            $cipher = "AES-256-CBC";
+                            $options = 0;
+                            $iv = str_repeat("0", openssl_cipher_iv_length($cipher));
+                            $encryptionKey = $user->keyAES;
+                            $decryptedName = openssl_decrypt($user->name, $cipher, $encryptionKey, $options, $iv);
+                            $decryptedImageAES = openssl_decrypt($user->imageBase64AES, $cipher, $encryptionKey, $options, $iv);
+                        ?>
+                        <img src="data:image/png;base64,{{ $decryptedImageAES }}" alt="User Image" style="width: 100%; height: 100%; object-fit: cover;">
+                        <h5 class="mb-0 user-name">{{$decryptedName}}</h5>
+                        <button class="btn btn-primary mt-2">Request Files</button>
+                    </div>
+            </div>
+        @endforeach
+    </div>
+@else
+    <p>You need to be logged in to view this page. Please <a href="/login">login</a>.</p>
+@endif
 
 @endsection
