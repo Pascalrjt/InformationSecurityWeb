e2f312859e9569df1eda2591fdd5b5ada0f7e4d6
Pascalrjt <pascal.rogerjt@gmail.com>
2023-11-23 22:46:03 +0700

    fixed positioning and code formatting

--- resources/views/inbox/index.blade.php
+++ resources/views/inbox/index.blade.php
@@ -5,14 +5,14 @@
 <style>
     .message-text {
         text-align: left;
-        padding: 10px;
+        padding: 10px 20px;
         display: flex;
         align-items: center;
+        justify-content: space-between;
     }
 
     .accept-button-container {
         text-align: right;
-        margin-top: 10px;
     }
 
     .message-card {
@@ -24,7 +24,7 @@
     <h1>Inbox</h1>
     <div class="row mx-5 justify-content-center">
         @foreach ($file_requests as $file_request)
-            <div class="card mx-2 bg-dark text-white message-card" style="width: 90%; height: 50px">
+            <div class="card mx-2 bg-dark text-white message-card" style="width: 90%; height: 60px">
                 <div class="message-text">
                     @if($file_request->has_access)
                         @if($file_request->requester_id === Auth::id())
@@ -35,18 +35,18 @@
                             <p>You dont have access to this request</p>
                         @endif
                     @else
-                    @if($file_request->requester_id === Auth::id() || $file_request->requested_id === Auth::id())
-                        <p>{{ \App\Models\User::find($file_request->requester_id)->username }} has requested access to your files</p>
-                        @if($file_request->requested_id === Auth::id())
-                            <div class="accept-button-container">
-                                <form method="POST" action="{{ route('filerequests.update', $file_request->id) }}">
-                                    @csrf
-                                    @method('PUT')
-                                    <button type="submit" class="btn btn-primary accept-button">Accept request</button>
-                                </form>
-                            </div>
-                    @endif
-                    @else
+                        @if($file_request->requester_id === Auth::id() || $file_request->requested_id === Auth::id())
+                            <p>{{ \App\Models\User::find($file_request->requester_id)->username }} has requested access to your files</p>
+                            @if($file_request->requested_id === Auth::id())
+                                <div class="accept-button-container">
+                                    <form method="POST" action="{{ route('filerequests.update', $file_request->id) }}">
+                                        @csrf
+                                        @method('PUT')
+                                        <button type="submit" class="btn btn-primary accept-button">Accept request</button>
+                                    </form>
+                                </div>
+                            @endif
+                        @else
                             <p>You dont have access to this request</p>
                         @endif
                     @endif
