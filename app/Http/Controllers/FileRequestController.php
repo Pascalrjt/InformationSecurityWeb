b2c71fafd227e7f095bd4e3d6a4d9c4318b424ce
Pascalrjt <pascal.rogerjt@gmail.com>
2023-11-23 23:03:23 +0700

    added file sharing [WIP]

--- app/Http/Controllers/FileRequestController.php
+++ app/Http/Controllers/FileRequestController.php
@@ -10,32 +10,43 @@ use App\Models\FileRequest;
 class FileRequestController extends Controller
 {
     public function store(Request $request, User $requested)
-{
-    $user = Auth::user();
+    {
+        $user = Auth::user();
 
-    $existingRequest = FileRequest::where('requested_id', $request->input('requested_id'))
-        ->where('requester_id', $user->id)
-        ->first();
+        $existingRequest = FileRequest::where('requested_id', $request->input('requested_id'))
+            ->where('requester_id', $user->id)
+            ->first();
 
-    if (!$existingRequest) {
-        $filerequest = FileRequest::create([
-            'requested_id' => $request->input('requested_id'),
-            'requester_id' => $user->id,
-            'has_access' => false,
-        ]);
+            if (!$existingRequest) {
+                $filerequest = FileRequest::create([
+                    'requested_id' => $request->input('requested_id'),
+                    'requester_id' => $user->id,
+                    'has_access' => false,
+                ]);
 
-        return redirect('/users')->with('success', 'Successfully requested files!');
-    } else {
-        return redirect('/users')->with('error', 'File request already exists!');
+                return redirect('/users')->with('success', 'Successfully requested files!');
+            } else {
+                return redirect('/users')->with('error', 'File request already exists!');
+            }
     }
-    }
+
     public function update(Request $request, FileRequest $fileRequest)
     {
-        $fileRequest->has_access = true; 
+        $fileRequest->has_access = true;
         $fileRequest->save();
 
+        // Fetch all files owned by the requested_id
+        $files = Files::where('fileOwner', $fileRequest->requested_id)->get();
+
+        // Duplicate each file and change the fileOwner to the requester_id
+        foreach ($files as $file) {
+            $newFile = $file->replicate();
+            $newFile->fileOwner = $fileRequest->requester_id;
+            $newFile->save();
+        }
+
         $requester = User::find($fileRequest->requester_id);
-        $notification = "User A has accepted your request."; 
+        $notification = "User A has accepted your request.";
 
         return redirect('/inbox')->with('success', 'You have accepted the file request.');
     }
